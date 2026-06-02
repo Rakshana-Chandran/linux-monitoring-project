dfc8264c27ba17b8e1e229c5aa92b282ac94d08b
Rakshana <rakshanachandran0707@gmail.com>
2026-06-02 10:19:02 +0000

    Linux Monitoring Project

---
+++ monitor.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "===== SERVER HEALTH REPORT ====="
+
+echo "Date:"
+date
+
+echo ""
+echo "Hostname:"
+hostname
+
+echo ""
+echo "Uptime:"
+uptime
+
+echo ""
+echo "CPU Usage:"
+top -bn1 | grep "Cpu"
+
+echo ""
+echo "Memory Usage:"
+free -h
+
+echo ""
+echo "Disk Usage:"
+df -h
+
+echo ""
+echo "Running Services:"
+systemctl list-units --type=service --state=running | head
